8fa6b99e82c0db931b6288255dc2aef7321b11f4
Patricia Meeremans <patriciameeremans@macbook-de-patricia.local>
2018-10-07 14:52:57 +0200

    Ajout de nouveaux controllers

---
+++ Les Calanques/Controller/ControllerAveTableView.swift
@@ -0,0 +1,30 @@
+//
+//  ControllerAveTableView.swift
+//  Les Calanques
+//
+//  Created by Patricia Meeremans on 07/10/2018.
+//  Copyright © 2018 Patricia Meeremans. All rights reserved.
+//
+
+import UIKit
+
+class ControllerAveTableView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
